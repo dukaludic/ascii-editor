f5babe8520c1c131352a85cece3052e202ac7b1d
dukaludic <dukaludic@gmail.com>
2024-10-02 14:59:38 +0200

    get matrix

--- index.js
+++ index.js
@@ -17,52 +17,41 @@ function getPerceivedLuminance(r, g, b) {
   return 0.2126 * r + 0.7152 * g + 0.0722 * b
 }
 
-// Dobijem koliki je cell u originalnim pixelima
-// 4 loopa: cell po cell su 2 i 2 su pixeli u cellu
+function getPixelMatrix(data) {
+  const matrix = new Array(height).fill().map(() => []);
+  let row = 0
+  
+  for (const pixel of data) {
+    if(matrix[row].length >= width) {
+      row++
+      matrix[row].push(pixel)
+    } else {
+      matrix[row].push(pixel)
+    }
+  }
 
-const cellSideSize = 1
+  return matrix
+}
 
-// for (let i = 0; i < height * channels; i += cellSideSize * channels) {
-//   for (let j = 0; j < width * channels; j += cellSideSize * channels) {
-//     for (
-//       let k = i * cellSideSize;
-//       k < i * cellSideSize + cellSideSize;
-//       k += 1 * channels
-//     ) {
-//       for (
-//         let l = j * cellSideSize;
-//         l < j * cellSideSize + cellSideSize;
-//         l += 1 * channels
-//       ) {
-//         console.log({ i, j, k, l })
-//       }
-//     }
-//   }
-// }
+const matrix = getPixelMatrix(data)
 
-// koliko treba pixela da spojim u jednu vrednost?
-// output ce da bude 50 x N za pocetak
-//  ratio treba da ostane isti
-// to znaci da treba da treba da odredim koliko pixela od input slike upada u 1/50 deo koji ce biti jedan character
-// jedan karater ce da bude 10 x 10 pixela
-// X input pixela loopujem i trazim sredisnje vrednosti da dobijem luminocity od grupe pixela
-// ako broj input pixela nije deljiv sa 50 ?
+console.log(matrix)
 
-// for (let i = 0; i < data.length; i += 3) {
-//   //   if (data[i] > 5) {
-//   data[i] = 255
-//   //   }
-// }
+const cellSide = 50 // ovo takodje treba da varira. Npr ako je slika manja da ne izgubi sve
 
-// for (let i = 0; i < height; i += cellSideSize) {
-//   for (let j = 0; j < width; j += cellSideSize) {
-//     for (let k = 0; k < ; k++) {
-//       for(let l = 0; k < data.length / height; l++) {
-//         console.log({i,j,k,l})
-//       }
-//     }
-//   }
-// }
+for (let i = 0; i < height; i+= cellSide) {
+  for (let j = 0; j < width; j+= cellSide) {
+    for (let k = i; k < i + cellSide; k++) {
+      for (let l = j; l < j + cellSide; l++) {
+        console.log({i,j,k,l})
+        
+      }
+      
+    }
+    
+  }
+  
+}
 
 await sharp(data, { raw: { width, height, channels } })
   .toFormat('jpg')
